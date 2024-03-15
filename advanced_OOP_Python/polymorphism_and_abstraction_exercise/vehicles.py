5f576175a4e450ba2985d51f8f254c31f2a767f2
Borislav Kurdzhiev <144941713+Borislav-Kurdzhiev@users.noreply.github.com>
2024-03-15 19:49:13 +0200

    Add files via upload

---
+++ advanced_OOP_Python/polymorphism_and_abstraction_exercise/vehicles.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+
+
+class Vehicle(ABC):
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    @abstractmethod
+    def drive(self, distance: float) -> None:
+        ...
+
+    @abstractmethod
+    def refuel(self, fuel: float) -> None:
+        ...
+
+
+class Car(Vehicle):
+    CONDITIONER_ON: float = 0.9
+
+    def drive(self, distance: float) -> None:
+        consumption = (self.CONDITIONER_ON + self.fuel_consumption) * distance
+
+        if self.fuel_quantity >= consumption:
+            self.fuel_quantity -= consumption
+
+    def refuel(self, fuel: float) -> None:
+        self.fuel_quantity += fuel
+
+
+class Truck(Vehicle):
+    CONDITIONER_ON: float = 1.6
+    TANK_PERCENTAGE_FUEL: float = 0.95
+
+    def drive(self, distance: float) -> None:
+        consumption = (self.CONDITIONER_ON + self.fuel_consumption) * distance
+
+        if self.fuel_quantity >= consumption:
+            self.fuel_quantity -= consumption
+
+    def refuel(self, fuel: float) -> None:
+        self.fuel_quantity += fuel * self.TANK_PERCENTAGE_FUEL
+
+
+car = Car(20, 5)
+car.drive(3)
+print(car.fuel_quantity)
+car.refuel(10)
+print(car.fuel_quantity)
