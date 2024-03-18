6c28e517335a03114f86787ffe3244d9593e41da
Borislav Kurdzhiev <144941713+Borislav-Kurdzhiev@users.noreply.github.com>
2024-03-19 00:17:23 +0200

    Add files via upload

---
+++ advanced_OOP_Python/solid/print_book.py
@@ -0,0 +1,20 @@
+class Book:
+    def __init__(self, content: str):
+        self.content = content
+
+
+class Formatter:
+    def format(self, book: Book) -> str:
+        return book.content
+
+
+class Printer:
+    def get_book(self, book: Book, formatter):
+        return formatter.format(book)
+
+
+b = Book('books')
+f = Formatter()
+p = Printer()
+
+print(p.get_book(b, f))
